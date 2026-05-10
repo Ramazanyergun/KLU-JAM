9787a2001f6d3c5fc1cf424a878cd442d3f9fc8c
Ramazanyergun <yergunr@gmail.com>
2026-05-10 13:44:52 +0300

    Sound and Environment

--- Assets/_Game/Scripts/Core/Manager/SoundManager.cs
+++ Assets/_Game/Scripts/Core/Manager/SoundManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio; // En üste ekle
+using UnityEngine.UI;    // Sliderlar için ekle 
 public class SoundManager : MonoBehaviour
 {
 [SerializeField] AudioSource musicSource;
 [SerializeField] AudioSource SFXSource;
-
+[SerializeField] private AudioMixer mainMixer;
 public AudioClip button;
 public AudioClip backgroundmusic;
 public AudioClip attack;
@@ -21,21 +23,39 @@ public AudioClip warriordeath;
 public AudioClip witchdeath;
    public static SoundManager Instance;
 
-    private void Awake()
+  private void Awake()
+{
+    // Singleton yapısı
+    if (Instance == null)
     {
-        // Singleton Yapısı: Sahneler arası geçişte tek bir SoundManager kalmasını sağlar
-        if (Instance == null)
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        // --- BAĞLANTIYI BURADA KODLA YAPIYORUZ ---
+        if (mainMixer != null)
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
+            // 1. Mixer'ın içindeki "Music" isimli grubu bul
+            var musicGroups = mainMixer.FindMatchingGroups("Music");
+            if (musicGroups.Length > 0) 
+            {
+                // 2. Bulduğun grubu musicSource'un OUTPUT kısmına ata
+                musicSource.outputAudioMixerGroup = musicGroups[0];
+            }
+
+            // 3. Mixer'ın içindeki "SFX" isimli grubu bul
+            var sfxGroups = mainMixer.FindMatchingGroups("SFX");
+            if (sfxGroups.Length > 0) 
+            {
+                // 4. Bulduğun grubu SFXSource'un OUTPUT kısmına ata
+                SFXSource.outputAudioMixerGroup = sfxGroups[0];
+            }
         }
     }
-
+    else
+    {
+        Destroy(gameObject);
+    }
+}
     private void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -78,5 +98,24 @@ public AudioClip witchdeath;
             case "Fare": PlaySFX(bocukdeath); break;
             case "Cadi": PlaySFX(witchdeath); break;
         }
+    }
+  
+
+    // Music Slider için fonksiyon
+    public void SetMusicVolume(float value)
+    {
+        Debug.Log("Müzik Slider Değeri: " + value); // Console'da bunu görüyor musun?
+    
+    if (mainMixer != null)
+    {
+        bool result = mainMixer.SetFloat("MusicVol", Mathf.Log10(value) * 20);
+        Debug.Log("Mixer Parametresi Değişti mi?: " + result); 
+    }
+    }
+
+    // SFX Slider için fonksiyon
+    public void SetSFXVolume(float value)
+    {
+        mainMixer.SetFloat("SFXVol", Mathf.Log10(value) * 20);
     }
 }
---
+++ Assets/_Game/Scripts/Core/Volume/VolumeSettings.cs
@@ -0,0 +1,10 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+
+
+}
